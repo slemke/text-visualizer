2a14d72679fc5b8b1cc036b56f63d5dbabd0428d
Sascha Lemke <sascha-lemke@gmx.de>
2017-11-25 16:43:54 +0100

    removed old files from module index

--- visualization/modules/index.js
+++ visualization/modules/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/document', require('./documents/abstract.js'));
-router.use('/document', require('./documents/chapter.js'));
 router.use('/document', require('./documents/documents.js'));
-router.use('/document', require('./documents/paragraph.js'));
-router.use('/document', require('./documents/section.js'));
-router.use('/document', require('./documents/sentences.js'));
-router.use('/document', require('./documents/subsection.js'));
-router.use('/document', require('./documents/token.js'));
-
 router.use('/document', require('./length/length.js'));
 router.use('/document', require('./punctuation/punctuation.js'));
 router.use('/document', require('./stopwords/stopwords.js'));
